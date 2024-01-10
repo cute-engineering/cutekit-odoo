0709dc7c2b2513a8bba13923bbfe82de9106a032
keyboard-slayer <dalcjor07@gmail.com>
2024-01-10 14:57:00 +0100

    fix: switch from fish to bash for venv activation

--- meta/plugins/venv.py
+++ meta/plugins/venv.py
@@ -1,4 +1,4 @@
-from os import environ
+import os
 from cutekit import cli, model, shell, const
 from pathlib import Path
 
@@ -17,7 +17,15 @@ def _(args: cli.Args):
     cache["PYTHON_VERSION"] = args.consumeOpt("py-ver", cache.get("PYTHON_VERSION", "3.10"))
     cache["ODOO_VERSION"] = args.consumeOpt("ver", cache.get("ODOO_VERSION", "master"))
 
-    shell.exec("fish", "-C", f"source \"$PYENV_ROOT/versions/odoo-{cache['ODOO_VERSION']}/bin/activate.fish\"")
+    env_path = Path(os.environ["PYENV_ROOT"]) / "versions" / f"odoo-{cache['ODOO_VERSION']}"
+
+    if not env_path.exists():
+        env_path = Path(os.environ["PYENV_ROOT"]) / "versions" / f"odoo-{cache['ODOO_VERSION'].split('-')[0]}"
+        if not env_path.exists():
+            print("[-] Virtual environment not found")
+            return
+
+    shell.exec("bash", "--init-file", env_path / "bin" / "activate")
 
     cache.finilize()
 
@@ -28,7 +36,7 @@ def _(args: cli.Args):
     cache["PYTHON_VERSION"] = args.consumeOpt("py-ver", cache.get("PYTHON_VERSION", "3.11"))
     cache["ODOO_VERSION"] = args.consumeOpt("ver", cache.get("ODOO_VERSION", "master"))
 
-    pip_path = Path(environ["PYENV_ROOT"]) / "versions" / f"odoo-{cache['ODOO_VERSION']}" / "bin" / "pip3"
+    pip_path = Path(os.environ["PYENV_ROOT"]) / "versions" / f"odoo-{cache['ODOO_VERSION']}" / "bin" / "pip3"
 
     shell.exec("pyenv", "install", "-s", cache["PYTHON_VERSION"])
     shell.exec("pyenv", "virtualenv", "-f", cache["PYTHON_VERSION"], f"odoo-{cache['ODOO_VERSION']}")
@@ -37,6 +45,6 @@ def _(args: cli.Args):
 
     shell.exec(pip_path, "install", "-r", cache.rootdir / const.EXTERN_DIR / "odoo" / "odoo"/ "requirements.txt")
     shell.exec(pip_path, "install", "-r", Path(const.MODULE_DIR) / "requirements.txt")
-    shell.cpTree(const.MODULE_DIR, Path(environ["PYENV_ROOT"]) / "versions" / f"odoo-{cache['ODOO_VERSION']}" / "lib" / f"python{cache['PYTHON_VERSION']}" / "site-packages" / "cutekit")
+    shell.cpTree(const.MODULE_DIR, Path(os.environ["PYENV_ROOT"]) / "versions" / f"odoo-{cache['ODOO_VERSION']}" / "lib" / f"python{cache['PYTHON_VERSION']}" / "site-packages" / "cutekit")
 
     cache.finilize()
